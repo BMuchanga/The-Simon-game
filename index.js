856733825f258d22548946b66c2eb449bdd7a4bc
Barney <cosmasmuchanga69@gmail.com>
2025-02-27 08:10:36 +0300

    project completion

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ var gamePattern=[];
 
 var userClickedPattern=[];
 
+var level=0;
+
+var started=false;
+
 $(".btn").click(function(){
     var userChosenColor=$(this).attr("id");
     userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
 })
 
 
 
 function nextSequence(){
+
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level "+level);
+
     var randomnumber=Math.floor(Math.random()*4);
     var randomChosenColor=buttonColors[randomnumber];
     gamePattern.push(randomChosenColor);
@@ -35,4 +48,40 @@ function animatePress(currentColor){
     setTimeout(function(){
         $("#"+currentColor).removeClass("pressed");
     },100);
+}
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started=true;
+    }
+    nextSequence();
+})
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success");
+        if(userClickedPattern.length===gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
 }
